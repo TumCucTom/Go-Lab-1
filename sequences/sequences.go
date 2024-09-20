c60331e6061794e3bc342fc349c63c5bfaf880ed
Thomas Bale <tokbale@outlook.com>
2024-09-20 12:13:55 +0100

    q3a

--- sequences/sequences.go
+++ sequences/sequences.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func addOne(a int) int {
 	return a + 1
 }
@@ -13,13 +15,24 @@ func double(slice []int) {
 }
 
 func mapSlice(f func(a int) int, slice []int) {
-
+		for i,item := range slice{
+			slice[i] = f(item)
+		}
 }
 
-func mapArray(f func(a int) int, array [3]int) {
-
+// skeleton modified here as arrays are passed by value not reference
+func mapArray(f func(a int) int, array [3]int) [3]int {
+	for i:=0;i<3;i++{
+		array[i] = f(array[i])
+	}
+	return array
 }
 
 func main() {
-
+	var intsArray = [3]int{1,2,3}
+	intsArray = mapArray(addOne,intsArray)
+	fmt.Println(intsArray)
+	intsSlice := []int{1,2,3}
+	mapSlice(addOne,intsSlice)
+	fmt.Println(intsSlice)
 }
